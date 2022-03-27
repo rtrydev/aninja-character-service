5cd91e102440c71937f0c2253638fd8fc3ceb463
Rafał Kulka <68755164+rtrydev@users.noreply.github.com>
2022-03-28 00:01:47 +0200

    Update CharacterWriteDto.cs

--- Dtos/CharacterWriteDto.cs
+++ Dtos/CharacterWriteDto.cs
@@ -1,12 +1,24 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace aninja_character_service.Dtos;
 
 public class CharacterWriteDto
 {
+    [Required]
+    [StringLength(50)]
     public string OriginalName { get; set; } = "";
+    [Required]
+    [StringLength(50)]
     public string TranslatedName { get; set; } = "";
+    [StringLength(100)]
     public string? ImageUrl { get; set; }
+    [DefaultValue("Female")]
     public string Gender { get; set; }
+    [Required]
+    [StringLength(50)]
     public string VoiceActor { get; set; } = "";
+    [StringLength(50)]
     public string? VoiceActorImageUrl { get; set; }
+    [StringLength(1000)]
     public string Description { get; set; } = "";
 }
