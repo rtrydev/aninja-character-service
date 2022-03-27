97c854331f4e6ac53256f294a522463e672e94c0
rtrydev <r.kulka44@gmail.com>
2022-03-27 01:01:42 +0100

    add character repo interface

--- Models/Anime.cs
+++ Models/Anime.cs
@@ -2,5 +2,8 @@ namespace aninja_character_service.Models;
 
 public class Anime
 {
-    
+    public int Id { get; set; }
+    public int ExternalId { get; set; }
+    public string? TranslatedTitle { get; set; }
+    public IEnumerable<Character>? Characters { get; set; }
 }
--- Repositories/ICharacterRepository.cs
+++ Repositories/ICharacterRepository.cs
@@ -1,6 +1,17 @@
+using aninja_character_service.Models;
+
 namespace aninja_character_service.Repositories;
 
 public interface ICharacterRepository
 {
+    public Task<IEnumerable<Character>?> GetCharactersForAnime(int animeId);
+    public Task<Character?> AddCharacterToAnime(int animeId, int characterId);
+
+    public Task<Character?> GetCharacterById(int characterId);
+    public Task<Character?> AddCharacter(Character character);
+
+    public Task<Anime?> AddAnime(Anime anime);
+    public Task<Anime?> GetAnimeById(int animeId);
+    public Task<bool> AnimeExists(int animeId);
     
 }
