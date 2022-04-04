2dd0a0184da0f6310fc1cc4c8dffce96650b0583
rtrydev <r.kulka44@gmail.com>
2022-04-04 17:31:36 +0200

    increase url size

--- Dtos/CharacterWriteDto.cs
+++ Dtos/CharacterWriteDto.cs
@@ -10,14 +10,14 @@ public class CharacterWriteDto
     [Required]
     [StringLength(50)]
     public string TranslatedName { get; set; } = "";
-    [StringLength(100)]
+    [StringLength(150)]
     public string? ImageUrl { get; set; }
     [DefaultValue("Female")]
     public string Gender { get; set; }
     [Required]
     [StringLength(50)]
     public string VoiceActor { get; set; } = "";
-    [StringLength(50)]
+    [StringLength(150)]
     public string? VoiceActorImageUrl { get; set; }
     [StringLength(1000)]
     public string Description { get; set; } = "";
